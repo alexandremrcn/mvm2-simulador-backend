d64438608a7cf6d8c5b0f1c2e2319da718c271f4
Alexandre Marcon <alexandre.mrcn@gmail.com>
2026-09-16 21:36:22 -0300

    add CORS versão 2

--- mvm2-rest-simulator/src/main/java/br/com/mvm2/controllers/Mvm2SimulatorController.java
+++ mvm2-rest-simulator/src/main/java/br/com/mvm2/controllers/Mvm2SimulatorController.java
@@ -1,9 +1,9 @@
 package br.com.mvm2.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,16 +15,17 @@ import mvm2services.models.SimulatorResult;
 import mvm2services.services.SimulatorService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/simulator")
 public class Mvm2SimulatorController {
 
-	@Autowired
-	private SimulatorService simulatorService;
+    @Autowired
+    private SimulatorService simulatorService;
 
-	@PostMapping
-	public Mvm2SimulatorResponseDTO simulate(@RequestBody Mvm2SimulatorRequestDTO request) throws Exception {
-		SimulatorInput input = Mvm2SimulatorMapper.toInput(request);
-		SimulatorResult result = simulatorService.simulate(input);
-		return Mvm2SimulatorMapper.toDTO(result);
-	}
+    @PostMapping
+    public Mvm2SimulatorResponseDTO simulate(@RequestBody Mvm2SimulatorRequestDTO request) throws Exception {
+        SimulatorInput input = Mvm2SimulatorMapper.toInput(request);
+        SimulatorResult result = simulatorService.simulate(input);
+        return Mvm2SimulatorMapper.toDTO(result);
+    }
 }
